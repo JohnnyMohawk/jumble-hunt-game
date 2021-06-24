b67b707b16dceb393cdaf0c11a38170e661a8078
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-06-23 22:59:27 -0500

    writes scrambler function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const wordsArr = ["PARTY","PARTYPANTS","PARTYHAT","PARTYSHOES","PARTYSOCKS","PAR
 let lettersArr = []
 let rndmNumIdx;
 let chosenWord;
+let scrambledWord;
 
 /*------------------------ Cached Element References ------------------------*/
 const board = document.getElementById('board')
@@ -12,3 +13,30 @@ const word = document.getElementById('theWord')
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function rndmIdxGen(){
+    rndmNumIdx = Math.floor(Math.random()*wordsArr.length)
+}
+
+function scrambler(){
+    chosenWord = wordsArr[rndmNumIdx]
+    wordLength = chosenWord.length
+    lettersArr = chosenWord.split("")
+    let curIdx = lettersArr.length
+    let tempVal
+    let rndmIdx
+    console.log("word length:", curIdx)
+    console.log("first:", lettersArr)
+    while (0 !== curIdx) {
+        rndmIdx = Math.floor(Math.random() * curIdx)
+        curIdx -= 1
+        tempVal = lettersArr[curIdx]
+        lettersArr[curIdx] = lettersArr[rndmIdx]
+        lettersArr[rndmIdx] = tempVal
+    }
+    scrambledWord = lettersArr.join("")
+    console.log(lettersArr)
+    console.log(scrambledWord)
+
+}
+
