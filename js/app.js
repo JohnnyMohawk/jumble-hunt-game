ecf582c96e7d67f4e840f145946e8c1c7bd4b460
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-06-23 20:42:16 -0500

    defines js constants and variables

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-/*-------------------------------- Constants --------------------------------*/
-
+const wordsArr = ["PARTY","PARTYPANTS","PARTYHAT","PARTYSHOES","PARTYSOCKS","PARTYUNDIES","PARTYSHIRT","PARTYDOWN"]
+let lettersArr = []
+let rndmNumIdx;
+let chosenWord;
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const board = document.getElementById('board')
+const unscrambled = document.getElementById('assemble')
+const word = document.getElementById('theWord')
 /*----------------------------- Event Listeners -----------------------------*/
 
 
